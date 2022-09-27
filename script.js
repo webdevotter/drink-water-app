6e691f964fd13d72b3116c934d2487ac4a28f686
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-26 20:51:42 -0500

    javascript
    need to figure out how to show 'remained'

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup
+
 smallCups.forEach((cup, idx)=>{
     cup.addEventListener('click', () => highlightCups(idx))
 })
@@ -11,7 +13,7 @@ function highlightCups(idx){
     if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--
     }
-    
+
     smallCups.forEach((cup, idx2 ) => {
         if(idx2 <= idx) {
             cup.classList.add('full')
@@ -19,4 +21,29 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.small-cup.full').length
+    
+    const totalCups = smallCups.length
+
+    if(fullCups === 0){
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups/totalCups*330}px`
+        percentage.innerText = `${fullCups/totalCups *100}%`
+    }
+
+    if(fullCups === totalCups){
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        listers.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
 }
